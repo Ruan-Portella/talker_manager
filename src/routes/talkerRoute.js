9146f89b2937785fddafa4c492d3ea3bf747c49f
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-19 15:08:01 -0300

    Feat: Resolve Exercicio 02

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const { readFile } = require('../utils/readFile');
+const { readFile, readFileById } = require('../utils/readFile');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
         const talkers = await readFile();
-        console.log(talkers);
-
         if (talkers.length === 0) return res.status(200).json([]);
         return res.status(200).json(talkers);
     } catch (error) {
         return res.status(500).json({message: {error}})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const talkers = await readFileById(Number(req.params.id));
+        if (talkers.length === 0) {
+         throw new Error('Pessoa palestrante não encontrada')
+        }
+        return res.status(200).json(...talkers);
+    } catch {
+        return res.status(404).json({
+            "message": 'Pessoa palestrante não encontrada'
+          })
     }
 })
 
--- src/utils/readFile.js
+++ src/utils/readFile.js
@@ -6,11 +6,20 @@ const talkerPath = path.resolve(__dirname, '../talker.json');
 const readFile = async () => {
     try {
         const data = await fs.readFile(talkerPath);
-
         return JSON.parse(data)
     } catch (error) {
         console.error('Não foi possível ler o arquivo');
     }
 }
 
-module.exports = { readFile }
+const readFileById = async (id) => {
+    try {
+        const talkers = await readFile();
+        const filterById = talkers.filter((talker) => talker.id === id)
+        return filterById;
+    } catch (error) {
+        console.error('Não foi possível ler o arquivo');
+    }
+}
+
+module.exports = { readFile, readFileById }
